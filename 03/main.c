2903de7f753d1d430974c0065ec57157e561e0fe
0x35c <unai64@orange.fr>
2024-05-30 17:17:14 +0200

    assignment 01 to 04 done

---
+++ 03/main.c
@@ -0,0 +1,32 @@
+#include <linux/delay.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+
+MODULE_LICENSE("GPL");
+
+static int do_work(int time)
+{
+	int x;
+	int z;
+
+	for (x = 0; x < time; ++x)
+		udelay(10);
+	if (time > 10)
+		pr_info("We slept a long time!");
+	z = x * time;
+	return z;
+}
+
+static __init int my_init(void)
+{
+	do_work(10);
+	return 0;
+}
+
+staitc __exit void my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
